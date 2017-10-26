a084d83fa56bd57635e39396a646ca669249999e
leoneljerez <leoneljerez1022@gmail.com>
2017-10-26 15:55:00 +0000

    hw4-part4

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -1,18 +1,33 @@
 # Add a declarative step here for populating the DB with movies.
-value = 0
+
 Given /the following movies exist/ do |movies_table|
-  value = 0
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
-    Movie.create(movie)
-    value += 1
+    Movie.create!(movie)
   end
-  fail "Unimplemented"
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
   Movie.count.should be n_seeds.to_i
+end
+
+Then (/^I should see: "([^"]*)"$/) do |regexp|
+  regexp = Regexp.new(regexp)
+
+  if page.respond_to? :should
+    page.should have_xpath('//*', :text => regexp)
+  else
+    assert page.has_xpath?('//*', :text => regexp)
+  end
+end
+
+Then (/^I should not see: "([^"]*)"$/) do |text|
+  if page.respond_to? :should
+    page.should have_no_content(text)
+  else
+    assert page.has_no_content?(text)
+  end
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -21,9 +36,8 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  match = /#{e1}.*#{e2}/m =~ page.body
-  assert !match.nil?
-  fail "Unimplemented"
+  movies_shown = page.all("#movies tbody td:nth-child(1)").map { |m| m.text }
+  (movies_shown.index("#{e1}") < movies_shown.index("#{e2}")).should be true
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -35,20 +49,19 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
   rating_list.delete!("\"")
-  if uncheck.nil?
-    ratings_list.split(',').each do |field|
-      check("ratings["+field.strip+"]")
-    end
-  else
-    ratings_list.split(',').each do |field|
-      uncheck("ratings["+field.strip+"]")
+    if uncheck.nil?
+      rating_list.split(',').each do |field|
+        check("ratings["+field.strip+"]")
+      end
+    else
+       rating_list.split(',').each do |field|
+        uncheck("ratings["+field.strip+"]")
     end
   end
-  fail "Unimplemented"
 end
 
-Then /I should see all the movies/ do
+Then /I should see all of the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  page.should have_css("table#movies tbody tr", :count => value.to_i)
-  fail "Unimplemented"
+  movies_shown = page.all("#movies tbody td:nth-child(1)").map { |m| m.text }
+  movies_shown.count.should == Movie.count
 end
